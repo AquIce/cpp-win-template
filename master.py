c96e36645d8931651dbe275fc7e9efad693cc687
AquIce <timeo.perrinjaquet@gmail.com>
2025-07-05 11:27:42 +0200

    Added better build/run script

--- master.py
+++ master.py
@@ -4,38 +4,52 @@ import sys
 if len(sys.argv) != 2:
     print(
         '''Usage :
+        python master.py cmake 
+        python master.py make
         python master.py build 
         python master.py run
-        python master.py brun
+        python master.py all 
         python master.py binlist'''
     )
     exit(1)
 
+def cmake():
+    return os.system('cmake -S . -B build -G "Unix Makefiles"')
+
+def make():
+    return os.system('cd build && make')
+
 def build():
-    os.system('cmake -S . -B build -G "Unix Makefiles"')
-    os.system('cd build && make')
+    err = cmake()
+    if err != 0:
+        return err
+
+    return make()
 
 def run():
     if sys.platform == 'linux':
-        os.system('cd build/bin && ./cpp-win-template')
-    else:
-        os.system('cd build/bin && .\\cpp-win-template')
+        return os.system('cd build/bin && ./cpp-win-template')
+    return os.system('cd build/bin && .\\cpp-win-template')
+
+def all():
+    err = build()
+    if err != 0:
+        return err
+    return run()
 
 def binlist():
     print('\n'.join([f'- {item}' for item in os.listdir('build/bin')])) 
 
+args = {
+    'cmake': cmake,
+    'make': make,
+    'build': build,
+    'run': run,
+    'all': all,
+    'binlist': binlist
+}
+
 action = sys.argv[1]
 
-if action == 'build':
-    build()
-    exit(0)
-if action == 'run':
-    run()
-    exit(0)
-if action == 'brun':
-    build()
-    run()
-    exit(0)
-if action == 'binlist':
-    binlist()
+args[action]()
 
